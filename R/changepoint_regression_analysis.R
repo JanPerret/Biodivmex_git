a907d20c46318c1843cca8cd81328aa6f6f0a35f
JanPerret <jan.perret@gmail.com>
2021-02-04 12:16:55 +0100

    add coefficients to changepoint regression output, and remove lumbricina

--- R/changepoint_regression_analysis.R
+++ R/changepoint_regression_analysis.R
@@ -12,21 +12,23 @@ GenBank_taxa_trend <- read_csv2("./output/text/GenBank_all_taxa_year_tab_acc.csv
 # WOS_taxa_trend <- read_csv2("./output/text/WOS_recap_table_articles_per_year.csv")
 # GenBank_taxa_trend <- read_csv2("./output/text/GenBank_all_taxa_year_tab.csv")
 
-GenBank_taxa_trend <- GenBank_taxa_trend[, -c(2,3,4)] # drop years 1987 to 1989
+# drop years 1987 to 1989
+GenBank_taxa_trend <- GenBank_taxa_trend[, -c(2,3,4)]
 
+# drop lumbricina
+WOS_taxa_trend <- subset(WOS_taxa_trend, WOS_taxa_trend$taxa != "lumbricina")
 GenBank_taxa_trend <- subset(GenBank_taxa_trend, GenBank_taxa_trend$taxa != "lumbricina")
 
-# mydata = GenBank_taxa_trend
-# mytaxa = "plant"
 
+# function to perform the changepoint regression analysis
 get_breakpoints <- function(mydata) {
   
   taxa_vect <- unique(mydata$taxa)
   year1990 <- c(1:30)
   empty_vect <- rep(NA, nrow(mydata))
   result_table <- data.frame(taxa = empty_vect, breakpoint = empty_vect, lowerCI = empty_vect,
-                             upperCI = empty_vect, p_value = empty_vect)
-  
+                             upperCI = empty_vect, p_value = empty_vect, intercept = empty_vect,
+                             slope1 = empty_vect, slope2 = empty_vect)
   index <- 0
   
   for (mytaxa in taxa_vect) {
@@ -40,7 +42,7 @@ get_breakpoints <- function(mydata) {
     # fit regression
     fit.glm <- glm(n_references ~ year1990, data = taxa_tab, family = "quasipoisson") # with a poisson glm
     
-    # # plot this model
+    # plot this model
     # x0 <- seq(min(year1990), max(year1990), length = 30)  # prediction grid
     # y0 <- predict.glm(fit.glm, newdata = list(x = x0))  # predicted values
     # y0 <- exp(y0) # backtransform estimates
@@ -61,11 +63,17 @@ get_breakpoints <- function(mydata) {
     lowerCI <- 1990 + round(myconfint[2], digits = 2) # lower IC
     upperCI <- 1990 + round(myconfint[3], digits = 2) # upper IC
     
+    # get segmented regression coefficients
+    mycoef <- coef(fit.seg)
+    myintercept <- mycoef[[1]]
+    myslope1 <- mycoef[[2]]
+    myslope2 <- mycoef[[2]] + mycoef[[3]]
+    
     # get davies test p-values
     mytest <- pscore.test(fit.glm, ~ year1990, k = 30) # test for the existence of one breakpoint
     myp_value <- mytest$p.value
     
-    result_vect <- c(taxa_tab$taxa[1], breakpoint, lowerCI, upperCI, myp_value)
+    result_vect <- c(taxa_tab$taxa[1], breakpoint, lowerCI, upperCI, myp_value, myintercept, myslope1, myslope2)
     
     result_table[index, ] <- result_vect
     
@@ -74,47 +82,10 @@ get_breakpoints <- function(mydata) {
   return(result_table)
 }
 
+# perform the changepoint analysis
 WOS_breakpoints <- get_breakpoints(mydata = WOS_taxa_trend)
 GenBank_breakpoints <- get_breakpoints(mydata = GenBank_taxa_trend)
 
-
-### fit segmented regression for lumbricina separately because the excess of zeros induces convergence error
-GenBank_taxa_trend <- read_csv2("./output/text/GenBank_all_taxa_year_tab_acc.csv")
-lumbri_tab <- subset(GenBank_taxa_trend, GenBank_taxa_trend$taxa == "lumbricina")
-lumbri_tab <- lumbri_tab[, -c(2:14)] # drop years 1987 to 1989
-
-lumbri_tab <- lumbri_tab %>%
-  pivot_longer(cols = "2000":"2019", names_to = "year", values_to = "n_references")
-year2000 <- c(1:20)
-
-# fit the 2 regressions
-fit.glm <- glm(n_references ~ year2000, data = lumbri_tab, family = "quasipoisson") # with a poisson glm
-
-# # plot this model
-# x0 <- seq(min(year2000), max(year2000), length = 20)  # prediction grid
-# y0 <- predict.glm(fit.glm, newdata = list(x = x0))  # predicted values
-# y0 <- exp(y0) # backtransform estimates
-# plot(x = year2000, y = lumbri_tab$n_references)
-# lines(x0, y0, col = 2)  ## add regression curve (colour: red)
-
-fit.seg <- segmented(fit.glm, seg.Z = ~ year2000, npsi = 1)
-# seg.predict <- predict(fit.seg)
-# seg.predict <- exp(seg.predict) # backtransform estimates
-# lines(year2000, seg.predict, col = 2)
-# lines(fit.seg, col = 2, pch = 19, bottom = TRUE, lwd = 2) # for the CI for the breakpoint
-
-myconfint <- confint(fit.seg)
-
-breakpoint <- 2000 + round(myconfint[1], digits = 2) # breakpoint estimate
-lowerCI <- 2000 + round(myconfint[2], digits = 2) # lower IC
-upperCI <- 2000 + round(myconfint[3], digits = 2) # upper IC
-
-# get davies test p-values
-mytest <- pscore.test(fit.glm, ~ year2000, k = 30) # test for the existence of one breakpoint
-myp_value <- mytest$p.value
-
-result_vect <- c(lumbri_tab$taxa[1], breakpoint, lowerCI, upperCI, myp_value)
-GenBank_breakpoints <- rbind(GenBank_breakpoints[c(1:11), ], result_vect, GenBank_breakpoints[12, ])
-
+# save output
 write_csv2(WOS_breakpoints, file = "./output/text/WOS_breakpoints.csv")
 write_csv2(GenBank_breakpoints, file = "./output/text/GenBank_breakpoints.csv")
